319776b40302539e2f46cb123a435e5c5344cede
Bruno <counter_19_95@hotmail.com>
2023-06-28 06:59:54 +0900

    added test bingo card view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'room', loadChildren: () => import('./features/room/room.module').then(m => m.RoomModule) }];
+const routes: Routes = [
+  {
+    path: '', redirectTo: '/room', pathMatch: 'full'
+  },
+  {
+    path: 'room',
+    loadChildren: () =>
+      import('./features/room/room.module').then((m) => m.RoomModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
