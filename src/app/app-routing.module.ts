4466541e8c8a29ba8fabfa4691ec8483544e1ea5
Bruno <counter_19_95@hotmail.com>
2023-06-28 19:06:39 +0900

    added style bingo room semiterminado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LobbyComponent } from './features/room/lobby/lobby.component';
+import { RoomComponent } from './features/room/room.component';
+import { BingoRoomComponent } from './features/room/bingo-room/bingo-room.component';
 
 const routes: Routes = [
+  {path: 'lobby', component: LobbyComponent},
+  {path: 'room', component: RoomComponent},
+  {path: 'principal', component: BingoRoomComponent},
   {
-    path: '', redirectTo: '/room', pathMatch: 'full'
+    path: '', redirectTo: '/bingo_room', pathMatch: 'full'
   },
   {
-    path: 'room',
+    path: 'bingo_room',
     loadChildren: () =>
       import('./features/room/room.module').then((m) => m.RoomModule),
+  },
+  {
+    path: '**', redirectTo: 'bingo_room', pathMatch:'full'
   },
 ];
 
